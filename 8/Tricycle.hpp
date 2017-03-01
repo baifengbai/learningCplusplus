95a8dd8ea842051d6a631d959030b6e69195d347
Liz Baillie <liz@lizbaillie.com>
2017-03-01 08:04:28 -0800

    Chapter 9, part 1: Moving into advanced classes

---
+++ 8/Tricycle.hpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+class Tricycle
+{
+public:
+    Tricycle(int initialSpeed);
+    ~Tricycle();
+    int getSpeed() const { return speed; }
+    void setSpeed(int speed);
+    void pedal()
+    {
+        setSpeed(speed + 1);
+        std::cout << "Pedaling " << getSpeed() << " mph\n\n";
+    }
+    void brake()
+    {
+      setSpeed(speed - 1);
+      std::cout << "Pedaling " << getSpeed() << " mph\n\n";
+    }
+private:
+    int speed;
+};
